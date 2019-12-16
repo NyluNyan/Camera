a6bb1733cca267d8b440706f7819960d2254828b
NyluNyan <39504014+NyluNyan@users.noreply.github.com>
2019-12-15 20:03:15 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require('fs'),
+path = require('path');
+let config = require('./config.json');
+
+module.exports = function AutoCamera(mod) {
+	
+	mod.command.add('cam', cmd =>
+	{
+		if(!isNaN(cmd))
+		{
+			Set(cmd);
+			config.distance = cmd;
+			mod.command.message('distance set to: ' + cmd);
+		}
+		else
+		{
+			config.enable=!config.enable;
+			mod.command.message('camera distance ' + (config.enable ? 'en' : 'dis') + 'abled');
+		}
+		fs.writeFileSync(path.join(__dirname, './config.json'), JSON.stringify(config));
+	});
+	
+	mod.game.on('leave_loading_screen', () => {
+        if(config.enable) Set(config.distance);
+    });
+
+	function Set(distance)
+	{
+		mod.toClient('S_DUNGEON_CAMERA_SET', 1, {
+			enabled: true,
+			default: Math.floor(distance/1.5),
+			max: distance
+		});
+	}
+}
